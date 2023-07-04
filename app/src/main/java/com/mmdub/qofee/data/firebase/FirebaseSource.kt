7e5b1c086db47a9c83734101255f0f0d0a11f644
fahmirumagutawan <fahmigutawan@gmail.com>
2023-07-04 11:34:53 +0700

    Add firestore to DI

--- app/src/main/java/com/mmdub/qofee/data/firebase/FirebaseSource.kt
+++ app/src/main/java/com/mmdub/qofee/data/firebase/FirebaseSource.kt
@@ -1,8 +1,9 @@
 package com.mmdub.qofee.data.firebase
 
+import com.google.firebase.firestore.FirebaseFirestore
 import javax.inject.Inject
 
 class FirebaseSource @Inject constructor(
-
+    firestore:FirebaseFirestore
 ) {
 }
--- app/src/main/java/com/mmdub/qofee/di/FirebaseModule.kt
+++ app/src/main/java/com/mmdub/qofee/di/FirebaseModule.kt
@@ -1,5 +1,8 @@
 package com.mmdub.qofee.di
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.mmdub.qofee.data.firebase.FirebaseSource
 import dagger.Module
 import dagger.Provides
@@ -12,5 +15,13 @@ import javax.inject.Singleton
 class FirebaseModule {
     @Provides
     @Singleton
-    fun provideFirebaseSource() = FirebaseSource()
+    fun provideFirebaseSource(
+        firestore:FirebaseFirestore
+    ) = FirebaseSource(
+        firestore = firestore
+    )
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore() = Firebase.firestore
 }
