71be3193df9307255458d464ea188e0619be5003
fahmirumagutawan <fahmigutawan@gmail.com>
2023-07-31 17:52:41 +0700

    Added splash screen

--- app/src/main/java/com/mmdub/qofee/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/mmdub/qofee/screen/splash/SplashScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,9 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -22,17 +26,67 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.mmdub.qofee.R
 import com.mmdub.qofee.ui.theme.AppColor
 import com.mmdub.qofee.util.NavRoutes
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    LaunchedEffect(key1 = true){
-        delay(2000)
-        navController.popBackStack()
-        navController.navigate(NavRoutes.HOME_SCREEN.name)
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+        AsyncImage(
+            modifier = Modifier.fillMaxSize(),
+            model = R.drawable.splash_screen_bg,
+            contentDescription = "",
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Qofee: E-Katalog",
+                    color = Color(0xffffffff),
+                    style = MaterialTheme.typography.displaySmall
+                )
+                Text(
+                    text = "Kopi Kalibaru",
+                    color = Color(0xffffffff),
+                    style = MaterialTheme.typography.displaySmall
+                )
+            }
+
+            Text(
+                text = "Rasakan nikmatnya kopi khas Banyuwangi",
+                color = Color(0xffFFDDB9),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    navController.popBackStack()
+                    navController.navigate(NavRoutes.HOME_SCREEN.name)
+                },
+                shape = RoundedCornerShape(14.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xffFFDDB9),
+                    contentColor = Color(0xff000000)
+                )
+            ) {
+                Text(text = "Mulai Sekarang")
+            }
+        }
     }
 }
